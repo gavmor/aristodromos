c5b22d7a1afda35c88c4e3f0316e81fcaac6a5bf
Gavin Morgan <morgan.gavin@gmail.com>
2024-12-12 18:21:53 -0800

    cut to the chase

--- wxt.config.ts
+++ wxt.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'wxt';
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
+  runner: {
+    startUrls: ["https://chooseyourstory.com/story/viewer/default.aspx?StoryId=11246"]
+  },
   extensionApi: 'chrome',
   modules: ['@wxt-dev/module-react'],
 });
