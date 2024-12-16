0238e89fc9b661fa172e13a1ccf961cb6ab29c74
Gavin Morgan <morgan.gavin@gmail.com>
2024-12-16 15:43:39 -0800

    use firefox-nightly
    turns out those were arbitrary keys, not executables. I am sensing a pattern. One great risk with a "framework" is that its abstractions depart from (while masquerading as) runtimes and syntax with reliable tooling.

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,8 +3,11 @@ import { defineConfig } from 'wxt';
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   runner: {
+    binaries: {
+      firefox: 'firefox-nightly',
+    },
     startUrls: ["https://chooseyourstory.com/story/viewer/default.aspx?StoryId=11246"]
   },
-  extensionApi: 'chrome',
+  extensionApi: 'webextension-polyfill',
   modules: ['@wxt-dev/module-react'],
 });
